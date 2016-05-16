9eadb71289766208a96f55f9a73222bebc6b93e4
Néstor Coppi <nestorcoppi@gmail.com>
2016-05-16 01:50:32 -0300

    Desesperación maxima

--- configs.js
+++ configs.js
@@ -1,23 +1,50 @@
 var inputs = document.querySelectorAll('input'),
-    i;
+    i,
+
+    saveConfig = function(data) {
+        chrome.storage.sync.set(data, function() {
+            var _status = document.getElementById('status');
+
+            _status.textContent = 'Options saved.';
+
+            setTimeout(function() {
+                _status.textContent = '';
+            }, 2000);
+        });
+    },
+
+    restoreConfig = function(input) {
+        var field = input.name;
+
+        chrome.storage.sync.get('tuentiUanConfig', function(config) {
+            value = config['tuentiUanConfig'][field] || '';
+
+            if (value) {
+                if (input.type === 'checkbox') {
+                    input.checked = value;
+                }
+
+                input.value = value;
+            }
+        });
+    };
 
 for (i = 0; i < inputs.length; i++) {
-    var input = inputs[i],
-        value = localStorage[input.name] || '';
+    var input = inputs[i];
 
-    if (input.type === 'checkbox') {
-        input.checked = value == 'true';
-    }
-    input.value = value;
+    restoreConfig(input);
 
     inputs[i].addEventListener('change', function(e){
-        var element = e.currentTarget,
+		var data = {tuentiUanConfig: {}},
+            element = e.currentTarget,
             value = element.value;
 
         if (element.type === 'checkbox') {
             value = element.checked;
         }
 
-        localStorage[element.name] = value;
+		data.tuentiUanConfig[element.name] = value;
+
+        saveConfig(data);
     });
 }
